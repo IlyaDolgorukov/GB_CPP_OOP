2499114b32a6961e7eeb0cf3f021a6cfd0b12e06
Ilya Dolgorukov <web@difabrik.ru>
2021-07-12 14:29:35 +0500

    blackjack3

--- blackjack/blackjack.cpp
+++ blackjack/blackjack.cpp
@@ -22,7 +22,21 @@ public:
     {
         return m_Rank < 10 ? m_Rank + 1 : 10;
     }
+    friend ostream& operator<< (ostream& out, const Card& card);
 };
+
+ostream& operator<< (ostream& out, const Card& card)
+{
+    if (card.m_IsFaceUp) {
+        string rnks[13] = {"ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"};
+        string sts[4] = {"spades", "hearts", "clubs", "diamonds"};
+        out << rnks[card.m_Rank] << " " << sts[card.m_Suit];
+    } else {
+        out << "XX";
+    }
+
+    return out;
+}
 
 class Hand
 {
@@ -72,6 +86,17 @@ public:
 
         return total;
     }
+    void FlipCard(int index)
+    {
+        if (m_Cards.size() > index) {
+            Card& card = *m_Cards.at(index);
+            card.Flip();
+        }
+    }
+    vector<Card*> GetCards() const
+    {
+        return m_Cards;
+    }
 };
 
 class GenericPlayer : public Hand
@@ -80,7 +105,7 @@ private:
     string name;
 public:
     GenericPlayer(string n): name(n) { }
-    virtual void IsHitting() = 0;
+    virtual bool IsHitting() = 0;
     bool IsBoosted()
     {
         return GetTotal() > 21;
@@ -89,6 +114,67 @@ public:
     {
         cout << "Player " << name << " has Boost!" << endl;
     }
+    string GetName() const
+    {
+        return name;
+    }
+    friend ostream& operator<< (ostream& out, const GenericPlayer& p);
+};
+
+ostream& operator<< (ostream& out, GenericPlayer& p)
+{
+    vector<Card*> cards = p.GetCards();
+    out << "Player " << p.GetName() << " cards: | ";
+    if (cards.size() > 0) {
+        vector<Card*>::iterator it;
+        for (it = cards.begin(); it != cards.end(); ++it) {
+            Card card = *(*it);
+            out << card << " | ";
+        }
+    }
+    out << endl << "Total scores: " << p.GetTotal();
+
+    return out;
+}
+
+class Player : public GenericPlayer
+{
+public:
+    Player(string n): GenericPlayer(n) { }
+    bool IsHitting()
+    {
+        string ans;
+        cout << "Do you wanna card? (y/n): ";
+        cin >> ans;
+
+        return ans == "y";
+    }
+    void Win() const
+    {
+        cout << "Player " << GetName() << " won!" << endl;
+    }
+    void Lose() const
+    {
+        cout << "Player " << GetName() << " lost!" << endl;
+    }
+    void Push() const
+    {
+        cout << "Player " << GetName() << " drew!" << endl;
+    }
+};
+
+class House : public GenericPlayer
+{
+public:
+    House(): GenericPlayer("House") { }
+    bool IsHitting()
+    {
+        return GetTotal() <= 16;
+    }
+    void FlipFirstCard()
+    {
+        FlipCard(0);
+    }
 };
 
 int main()
@@ -96,21 +182,24 @@ int main()
     Card card1(king, diamonds, false);
     Card card2(seven, clubs, false);
     Card card3(ten, hearts, false);
-    Card card4(ace, diamonds, true);
-    Card card5(queen, spades, true);
+    Card card4(ace, diamonds, false);
+    Card card5(queen, spades, false);
 
-    Hand player;
-    player.Add(&card1);
-    player.Add(&card2);
+    House house;
+    house.Add(&card1);
+    house.Add(&card2);
+    house.FlipFirstCard();
+    cout << house << endl;
+
+    Player player("John");
     player.Add(&card3);
     player.Add(&card4);
-    player.Add(&card5);
-
-    int points = player.GetTotal();
-    cout << "Player points = " << points << endl;
-    player.Clear();
-    points = player.GetTotal();
-    cout << "Player points = " << points << endl;
+    cout << player << endl;
+    if (player.IsHitting()) {
+        player.Add(&card5);
+        cout << player << endl;
+        player.Win();
+    }
 
     return 0;
 }
